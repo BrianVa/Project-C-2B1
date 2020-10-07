3f1b4811288fc067162266410b52a599cf1e1e34
Brian Vaartjes <brianvaartjes@gmail.com>
2020-10-07 18:31:04 +0200

    design change
    design change updated the color

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -20,7 +20,7 @@
 <!-- Site wrapper -->
 <div class="wrapper">
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <nav class="main-header navbar navbar-expand border-bottom navbar-dark bg-success">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
             <li class="nav-item">
@@ -82,9 +82,9 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-light-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="#" class="brand-link">
+        <a href="#" class="brand-link bg-success">
             <img src="{{ url('/img/AdminLTELogo.png') }}"
                  alt="AdminLTE Logo"
                  class="brand-image img-circle elevation-3"
